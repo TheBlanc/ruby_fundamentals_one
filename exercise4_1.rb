b3fe20e06475614069cdc027dcd7e6605776c54f
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-05-22 16:17:21 -0400

    complete exercise 4.1

---
+++ exercise4_1.rb
@@ -0,0 +1,8 @@
+puts "Hello, please enter a number."
+number = gets.to_i
+
+if number > 100
+  puts "Wow, #{number}? that's a big number!"
+else number < 100
+  puts "Only #{number}? why not dream bigger buddy?"
+end
