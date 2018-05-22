42bab45564e08df31050a0761fb79282aeeccf7c
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-05-22 18:08:10 -0400

    add functionality to go home

--- exercise5.rb
+++ exercise5.rb
@@ -1,7 +1,7 @@
 distance = 0
 run = true
 while run == true
-  puts "Would you like to walk or run or stop?"
+  puts "Would you like to walk or run or go home?"
   action = gets.chop
   if action.upcase == "RUN"
     distance += 5
@@ -9,7 +9,7 @@ while run == true
   elsif action.upcase == "WALK"
     distance += 1
     puts "Distance from home is #{distance}km."
-  elsif action.upcase == "STOP"
+  elsif action.upcase == "GO HOME"
     puts "All done. You ran #{distance}km!"
     run = false
   else
