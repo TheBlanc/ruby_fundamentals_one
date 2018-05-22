9fbbde5338bb3f3833330272d5f756a9b263eda5
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-05-22 15:12:35 -0400

    finish question 4

--- exercise2.rb
+++ exercise2.rb
@@ -6,3 +6,7 @@ puts "See you in " + 10.to_s
 
 #Question 3
 puts "45628 multiplied by 7839 is #{7839*45628}"
+
+#Question 4
+puts "Does (10 < 20 && 30 < 20) || !(10 == 11) equate to 'true'?"
+puts (10 < 20 && 30 < 20) || !(10 == 11)
